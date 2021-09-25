14854a2f57a4be8390407b196629c4c2ca69fbd1
Casimero Tanseco <tansecoc@flip1.engr.oregonstate.edu>
2021-09-25 11:50:49 -0700

    Updated app.js to point to database.

--- app.js
+++ app.js
@@ -15,7 +15,34 @@ var db = require('./database/db-connector')
 */
 app.get('/', function(req, res)                 // This is the basic syntax for what is called a 'route'
     {
-        res.send("The server is running! See this? This is Cas's port! :)")      // This function literally sends the string "The server is running!" to the computer
+        // res.send("The server is running! See this? This is Cas's port! :)")      // This function literally sends the string "The server is running!" to the computer
+
+        // Define our queries
+        query1 = 'DROP TABLE IF EXISTS diagnostic;';
+        query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
+        query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working!")';
+        query4 = 'SELECT * FROM diagnostic;';
+
+        // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
+
+        // DROP TABLE...
+        db.pool.query(query1, function (err, results, fields){
+
+            // CREATE TABLE...
+            db.pool.query(query2, function(err, results, fields){
+
+                // INSERT INTO...
+                db.pool.query(query3, function(err, results, fields){
+
+                    // SELECT *...
+                    db.pool.query(query4, function(err, results, fields){
+
+                        // Send the results to the browser
+                        res.send(JSON.stringify(results));
+                    });
+                });
+            });
+        });
     });                                         // requesting the web site.
 
 /*
